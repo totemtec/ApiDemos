19a5da6f64fa608c2b3c2c27294097833fc511eb
Totem <majianglin@totemtec.com>
2015-04-23 19:39:23 +0800

    Complied By Android Studio

--- app/src/main/java/com/example/android/apis/os/MmsMessagingDemo.java
+++ app/src/main/java/com/example/android/apis/os/MmsMessagingDemo.java
@@ -16,19 +16,19 @@
 
 package com.example.android.apis.os;
 
-import com.google.android.mms.ContentType;
-import com.google.android.mms.InvalidHeaderValueException;
-import com.google.android.mms.pdu.CharacterSets;
-import com.google.android.mms.pdu.EncodedStringValue;
-import com.google.android.mms.pdu.GenericPdu;
-import com.google.android.mms.pdu.PduBody;
-import com.google.android.mms.pdu.PduComposer;
-import com.google.android.mms.pdu.PduHeaders;
-import com.google.android.mms.pdu.PduParser;
-import com.google.android.mms.pdu.PduPart;
-import com.google.android.mms.pdu.RetrieveConf;
-import com.google.android.mms.pdu.SendConf;
-import com.google.android.mms.pdu.SendReq;
+//import com.google.android.mms.ContentType;
+//import com.google.android.mms.InvalidHeaderValueException;
+//import com.google.android.mms.pdu.CharacterSets;
+//import com.google.android.mms.pdu.EncodedStringValue;
+//import com.google.android.mms.pdu.GenericPdu;
+//import com.google.android.mms.pdu.PduBody;
+//import com.google.android.mms.pdu.PduComposer;
+//import com.google.android.mms.pdu.PduHeaders;
+//import com.google.android.mms.pdu.PduParser;
+//import com.google.android.mms.pdu.PduPart;
+//import com.google.android.mms.pdu.RetrieveConf;
+//import com.google.android.mms.pdu.SendConf;
+//import com.google.android.mms.pdu.SendReq;
 
 import android.app.Activity;
 import android.app.PendingIntent;
@@ -43,8 +43,6 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
-import android.telephony.PhoneNumberUtils;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
@@ -55,13 +53,14 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.apis.R;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Random;
 
@@ -112,6 +111,8 @@ public class MmsMessagingDemo extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mms_demo);
+
+        Toast.makeText(this, "MMS Messaging is Disabled", Toast.LENGTH_LONG).show();
 
         // Enable or disable the broadcast receiver depending on the checked
         // state of the checkbox.
@@ -238,17 +239,17 @@ public class MmsMessagingDemo extends Activity {
         if (code == Activity.RESULT_OK) {
             final byte[] response = intent.getByteArrayExtra(SmsManager.EXTRA_MMS_DATA);
             if (response != null) {
-                final GenericPdu pdu = new PduParser(response).parse();
-                if (pdu instanceof SendConf) {
-                    final SendConf sendConf = (SendConf) pdu;
-                    if (sendConf.getResponseStatus() == PduHeaders.RESPONSE_STATUS_OK) {
-                        status = R.string.mms_status_sent;
-                    } else {
-                        Log.e(TAG, "MMS sent, error=" + sendConf.getResponseStatus());
-                    }
-                } else {
-                    Log.e(TAG, "MMS sent, invalid response");
-                }
+//                final GenericPdu pdu = new PduParser(response).parse();
+//                if (pdu instanceof SendConf) {
+//                    final SendConf sendConf = (SendConf) pdu;
+//                    if (sendConf.getResponseStatus() == PduHeaders.RESPONSE_STATUS_OK) {
+//                        status = R.string.mms_status_sent;
+//                    } else {
+//                        Log.e(TAG, "MMS sent, error=" + sendConf.getResponseStatus());
+//                    }
+//                } else {
+//                    Log.e(TAG, "MMS sent, invalid response");
+//                }
             } else {
                 Log.e(TAG, "MMS sent, empty response");
             }
@@ -281,16 +282,16 @@ public class MmsMessagingDemo extends Activity {
                 final byte[] response = new byte[nBytes];
                 final int read = reader.read(response, 0, nBytes);
                 if (read == nBytes) {
-                    final GenericPdu pdu = new PduParser(response).parse();
-                    if (pdu instanceof RetrieveConf) {
-                        final RetrieveConf retrieveConf = (RetrieveConf) pdu;
-                        mRecipientsInput.setText(getRecipients(context, retrieveConf));
-                        mSubjectInput.setText(getSubject(retrieveConf));
-                        mTextInput.setText(getMessageText(retrieveConf));
-                        status = R.string.mms_status_downloaded;
-                    } else {
-                        Log.e(TAG, "MMS received, invalid response");
-                    }
+//                    final GenericPdu pdu = new PduParser(response).parse();
+//                    if (pdu instanceof RetrieveConf) {
+//                        final RetrieveConf retrieveConf = (RetrieveConf) pdu;
+//                        mRecipientsInput.setText(getRecipients(context, retrieveConf));
+//                        mSubjectInput.setText(getSubject(retrieveConf));
+//                        mTextInput.setText(getMessageText(retrieveConf));
+//                        status = R.string.mms_status_downloaded;
+//                    } else {
+//                        Log.e(TAG, "MMS received, invalid response");
+//                    }
                 } else {
                     Log.e(TAG, "MMS received, empty response");
                 }
@@ -310,7 +311,7 @@ public class MmsMessagingDemo extends Activity {
     }
 
     public static final long DEFAULT_EXPIRY_TIME = 7 * 24 * 60 * 60;
-    public static final int DEFAULT_PRIORITY = PduHeaders.PRIORITY_NORMAL;
+//    public static final int DEFAULT_PRIORITY = PduHeaders.PRIORITY_NORMAL;
 
     private static final String TEXT_PART_FILENAME = "text_0.txt";
     private static final String sSmilText =
@@ -330,123 +331,124 @@ public class MmsMessagingDemo extends Activity {
 
     private static byte[] buildPdu(Context context, String recipients, String subject,
             String text) {
-        final SendReq req = new SendReq();
-        // From, per spec
-        final String lineNumber = getSimNumber(context);
-        if (!TextUtils.isEmpty(lineNumber)) {
-            req.setFrom(new EncodedStringValue(lineNumber));
-        }
-        // To
-        EncodedStringValue[] encodedNumbers =
-                EncodedStringValue.encodeStrings(recipients.split(" "));
-        if (encodedNumbers != null) {
-            req.setTo(encodedNumbers);
-        }
-        // Subject
-        if (!TextUtils.isEmpty(subject)) {
-            req.setSubject(new EncodedStringValue(subject));
-        }
-        // Date
-        req.setDate(System.currentTimeMillis() / 1000);
-        // Body
-        PduBody body = new PduBody();
-        // Add text part. Always add a smil part for compatibility, without it there
-        // may be issues on some carriers/client apps
-        final int size = addTextPart(body, text, true/* add text smil */);
-        req.setBody(body);
-        // Message size
-        req.setMessageSize(size);
-        // Message class
-        req.setMessageClass(PduHeaders.MESSAGE_CLASS_PERSONAL_STR.getBytes());
-        // Expiry
-        req.setExpiry(DEFAULT_EXPIRY_TIME);
-        try {
-            // Priority
-            req.setPriority(DEFAULT_PRIORITY);
-            // Delivery report
-            req.setDeliveryReport(PduHeaders.VALUE_NO);
-            // Read report
-            req.setReadReport(PduHeaders.VALUE_NO);
-        } catch (InvalidHeaderValueException e) {}
-
-        return new PduComposer(context, req).make();
+//        final SendReq req = new SendReq();
+//        // From, per spec
+//        final String lineNumber = getSimNumber(context);
+//        if (!TextUtils.isEmpty(lineNumber)) {
+//            req.setFrom(new EncodedStringValue(lineNumber));
+//        }
+//        // To
+//        EncodedStringValue[] encodedNumbers =
+//                EncodedStringValue.encodeStrings(recipients.split(" "));
+//        if (encodedNumbers != null) {
+//            req.setTo(encodedNumbers);
+//        }
+//        // Subject
+//        if (!TextUtils.isEmpty(subject)) {
+//            req.setSubject(new EncodedStringValue(subject));
+//        }
+//        // Date
+//        req.setDate(System.currentTimeMillis() / 1000);
+//        // Body
+//        PduBody body = new PduBody();
+//        // Add text part. Always add a smil part for compatibility, without it there
+//        // may be issues on some carriers/client apps
+//        final int size = addTextPart(body, text, true/* add text smil */);
+//        req.setBody(body);
+//        // Message size
+//        req.setMessageSize(size);
+//        // Message class
+//        req.setMessageClass(PduHeaders.MESSAGE_CLASS_PERSONAL_STR.getBytes());
+//        // Expiry
+//        req.setExpiry(DEFAULT_EXPIRY_TIME);
+//        try {
+//            // Priority
+//            req.setPriority(DEFAULT_PRIORITY);
+//            // Delivery report
+//            req.setDeliveryReport(PduHeaders.VALUE_NO);
+//            // Read report
+//            req.setReadReport(PduHeaders.VALUE_NO);
+//        } catch (InvalidHeaderValueException e) {}
+//
+//        return new PduComposer(context, req).make();
+        return null;
     }
 
-    private static int addTextPart(PduBody pb, String message, boolean addTextSmil) {
-        final PduPart part = new PduPart();
-        // Set Charset if it's a text media.
-        part.setCharset(CharacterSets.UTF_8);
-        // Set Content-Type.
-        part.setContentType(ContentType.TEXT_PLAIN.getBytes());
-        // Set Content-Location.
-        part.setContentLocation(TEXT_PART_FILENAME.getBytes());
-        int index = TEXT_PART_FILENAME.lastIndexOf(".");
-        String contentId = (index == -1) ? TEXT_PART_FILENAME
-                : TEXT_PART_FILENAME.substring(0, index);
-        part.setContentId(contentId.getBytes());
-        part.setData(message.getBytes());
-        pb.addPart(part);
-        if (addTextSmil) {
-            final String smil = String.format(sSmilText, TEXT_PART_FILENAME);
-            addSmilPart(pb, smil);
-        }
-        return part.getData().length;
-    }
-
-    private static void addSmilPart(PduBody pb, String smil) {
-        final PduPart smilPart = new PduPart();
-        smilPart.setContentId("smil".getBytes());
-        smilPart.setContentLocation("smil.xml".getBytes());
-        smilPart.setContentType(ContentType.APP_SMIL.getBytes());
-        smilPart.setData(smil.getBytes());
-        pb.addPart(0, smilPart);
-    }
-
-    private static String getRecipients(Context context, RetrieveConf retrieveConf) {
-        final String self = getSimNumber(context);
-        final StringBuilder sb = new StringBuilder();
-        if (retrieveConf.getFrom() != null) {
-            sb.append(retrieveConf.getFrom().getString());
-        }
-        if (retrieveConf.getTo() != null) {
-            for (EncodedStringValue to : retrieveConf.getTo()) {
-                final String number = to.getString();
-                if (!PhoneNumberUtils.compare(number, self)) {
-                    sb.append(" ").append(to.getString());
-                }
-            }
-        }
-        if (retrieveConf.getCc() != null) {
-            for (EncodedStringValue cc : retrieveConf.getCc()) {
-                final String number = cc.getString();
-                if (!PhoneNumberUtils.compare(number, self)) {
-                    sb.append(" ").append(cc.getString());
-                }
-            }
-        }
-        return sb.toString();
-    }
-
-    private static String getSubject(RetrieveConf retrieveConf) {
-        final EncodedStringValue subject = retrieveConf.getSubject();
-        return subject != null ? subject.getString() : "";
-    }
-
-    private static String getMessageText(RetrieveConf retrieveConf) {
-        final StringBuilder sb = new StringBuilder();
-        final PduBody body = retrieveConf.getBody();
-        if (body != null) {
-            for (int i = 0; i < body.getPartsNum(); i++) {
-                final PduPart part = body.getPart(i);
-                if (part != null
-                        && part.getContentType() != null
-                        && ContentType.isTextType(new String(part.getContentType()))) {
-                    sb.append(new String(part.getData()));
-                }
-            }
-        }
-        return sb.toString();
-    }
+//    private static int addTextPart(PduBody pb, String message, boolean addTextSmil) {
+//        final PduPart part = new PduPart();
+//        // Set Charset if it's a text media.
+//        part.setCharset(CharacterSets.UTF_8);
+//        // Set Content-Type.
+//        part.setContentType(ContentType.TEXT_PLAIN.getBytes());
+//        // Set Content-Location.
+//        part.setContentLocation(TEXT_PART_FILENAME.getBytes());
+//        int index = TEXT_PART_FILENAME.lastIndexOf(".");
+//        String contentId = (index == -1) ? TEXT_PART_FILENAME
+//                : TEXT_PART_FILENAME.substring(0, index);
+//        part.setContentId(contentId.getBytes());
+//        part.setData(message.getBytes());
+//        pb.addPart(part);
+//        if (addTextSmil) {
+//            final String smil = String.format(sSmilText, TEXT_PART_FILENAME);
+//            addSmilPart(pb, smil);
+//        }
+//        return part.getData().length;
+//    }
+//
+//    private static void addSmilPart(PduBody pb, String smil) {
+//        final PduPart smilPart = new PduPart();
+//        smilPart.setContentId("smil".getBytes());
+//        smilPart.setContentLocation("smil.xml".getBytes());
+//        smilPart.setContentType(ContentType.APP_SMIL.getBytes());
+//        smilPart.setData(smil.getBytes());
+//        pb.addPart(0, smilPart);
+//    }
+//
+//    private static String getRecipients(Context context, RetrieveConf retrieveConf) {
+//        final String self = getSimNumber(context);
+//        final StringBuilder sb = new StringBuilder();
+//        if (retrieveConf.getFrom() != null) {
+//            sb.append(retrieveConf.getFrom().getString());
+//        }
+//        if (retrieveConf.getTo() != null) {
+//            for (EncodedStringValue to : retrieveConf.getTo()) {
+//                final String number = to.getString();
+//                if (!PhoneNumberUtils.compare(number, self)) {
+//                    sb.append(" ").append(to.getString());
+//                }
+//            }
+//        }
+//        if (retrieveConf.getCc() != null) {
+//            for (EncodedStringValue cc : retrieveConf.getCc()) {
+//                final String number = cc.getString();
+//                if (!PhoneNumberUtils.compare(number, self)) {
+//                    sb.append(" ").append(cc.getString());
+//                }
+//            }
+//        }
+//        return sb.toString();
+//    }
+//
+//    private static String getSubject(RetrieveConf retrieveConf) {
+//        final EncodedStringValue subject = retrieveConf.getSubject();
+//        return subject != null ? subject.getString() : "";
+//    }
+//
+//    private static String getMessageText(RetrieveConf retrieveConf) {
+//        final StringBuilder sb = new StringBuilder();
+//        final PduBody body = retrieveConf.getBody();
+//        if (body != null) {
+//            for (int i = 0; i < body.getPartsNum(); i++) {
+//                final PduPart part = body.getPart(i);
+//                if (part != null
+//                        && part.getContentType() != null
+//                        && ContentType.isTextType(new String(part.getContentType()))) {
+//                    sb.append(new String(part.getData()));
+//                }
+//            }
+//        }
+//        return sb.toString();
+//    }
 
     private static String getSimNumber(Context context) {
         final TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(
--- app/src/main/java/com/example/android/apis/os/MmsWapPushReceiver.java
+++ app/src/main/java/com/example/android/apis/os/MmsWapPushReceiver.java
@@ -16,17 +16,9 @@
 
 package com.example.android.apis.os;
 
-import com.google.android.mms.ContentType;
-import com.google.android.mms.pdu.GenericPdu;
-import com.google.android.mms.pdu.NotificationInd;
-import com.google.android.mms.pdu.PduHeaders;
-import com.google.android.mms.pdu.PduParser;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.provider.Telephony;
-import android.util.Log;
 
 /**
  * Receiver for MMS WAP push
@@ -36,42 +28,42 @@ public class MmsWapPushReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Telephony.Sms.Intents.WAP_PUSH_RECEIVED_ACTION.equals(intent.getAction())
-                && ContentType.MMS_MESSAGE.equals(intent.getType())) {
-            final byte[] data = intent.getByteArrayExtra("data");
-            final PduParser parser = new PduParser(data);
-            GenericPdu pdu = null;
-            try {
-                pdu = parser.parse();
-            } catch (final RuntimeException e) {
-                Log.e(TAG, "Invalid MMS WAP push", e);
-            }
-            if (pdu == null) {
-                Log.e(TAG, "Invalid WAP push data");
-                return;
-            }
-            switch (pdu.getMessageType()) {
-                case PduHeaders.MESSAGE_TYPE_NOTIFICATION_IND: {
-                    final NotificationInd nInd = (NotificationInd) pdu;
-                    final String location = new String(nInd.getContentLocation());
-                    Log.v(TAG, "Received MMS notification: " + location);
-                    final Intent di = new Intent();
-                    di.setClass(context, MmsMessagingDemo.class);
-                    di.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                    di.putExtra(MmsMessagingDemo.EXTRA_NOTIFICATION_URL, location);
-                    context.startActivity(di);
-                    break;
-                }
-                // FLAG (ywen): impl. handling of the following push
-                case PduHeaders.MESSAGE_TYPE_DELIVERY_IND: {
-                    Log.v(TAG, "Received delivery report");
-                    break;
-                }
-                case PduHeaders.MESSAGE_TYPE_READ_ORIG_IND: {
-                    Log.v(TAG, "Received read report");
-                    break;
-                }
-            }
-        }
+//        if (Telephony.Sms.Intents.WAP_PUSH_RECEIVED_ACTION.equals(intent.getAction())
+//                && ContentType.MMS_MESSAGE.equals(intent.getType())) {
+//            final byte[] data = intent.getByteArrayExtra("data");
+//            final PduParser parser = new PduParser(data);
+//            GenericPdu pdu = null;
+//            try {
+//                pdu = parser.parse();
+//            } catch (final RuntimeException e) {
+//                Log.e(TAG, "Invalid MMS WAP push", e);
+//            }
+//            if (pdu == null) {
+//                Log.e(TAG, "Invalid WAP push data");
+//                return;
+//            }
+//            switch (pdu.getMessageType()) {
+//                case PduHeaders.MESSAGE_TYPE_NOTIFICATION_IND: {
+//                    final NotificationInd nInd = (NotificationInd) pdu;
+//                    final String location = new String(nInd.getContentLocation());
+//                    Log.v(TAG, "Received MMS notification: " + location);
+//                    final Intent di = new Intent();
+//                    di.setClass(context, MmsMessagingDemo.class);
+//                    di.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+//                    di.putExtra(MmsMessagingDemo.EXTRA_NOTIFICATION_URL, location);
+//                    context.startActivity(di);
+//                    break;
+//                }
+//                // FLAG (ywen): impl. handling of the following push
+//                case PduHeaders.MESSAGE_TYPE_DELIVERY_IND: {
+//                    Log.v(TAG, "Received delivery report");
+//                    break;
+//                }
+//                case PduHeaders.MESSAGE_TYPE_READ_ORIG_IND: {
+//                    Log.v(TAG, "Received read report");
+//                    break;
+//                }
+//            }
+//        }
     }
 }
